76ae442afa1a2888ab9ce85e27ff25c9aa71ff3c
Malith <malith.13@cse.mrt.ac.lk>
2015-05-21 11:12:51 +0530

    Brick game started

---
+++ Bricks3/src/Ball.java
@@ -0,0 +1,74 @@
+
+
+
+public class Ball {
+	
+	int x;
+	int y;
+	int w;
+	int h;
+	
+	int gameW;
+	int gameH;
+	
+	int dx;
+	int dy;
+	
+	int speed;
+	
+	
+	public Ball(int gameW, int gameH, int x, int y, int w, int h, int speed) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		
+		this.gameH = gameH;
+		this.gameW = gameW;
+		
+		this.speed = speed;
+		
+		this.dx = speed;
+		this.dy = speed;
+		
+		
+	}
+
+	public void update(){
+		this.x = x + dx;
+		this.y = y + dy;
+		
+		if(y > gameH-h){
+			dy = -dy;
+		}
+		if(x > gameW-w){
+			dx = -dx;
+		}
+		
+		if(y < 0){
+			dy = -dy;
+		}
+		if(x < 0){
+			dx = -dx;
+		}
+		
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getW() {
+		return w;
+	}
+
+	public int getH() {
+		return h;
+	}
+
+
+}
